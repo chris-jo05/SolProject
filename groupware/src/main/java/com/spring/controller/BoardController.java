0fb7e4e15757e6e94870bf1b0dfec63adb13bfc9
hyundojo <rela1593@gmail.com>
2021-07-07 16:18:30 +0900

    한글 깨지는 현상 수정

--- groupware/src/main/java/com/spring/controller/BoardController.java
+++ groupware/src/main/java/com/spring/controller/BoardController.java
@@ -11,7 +11,6 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/board/*")
 public class BoardController {
 
-<<<<<<< HEAD
 	@GetMapping("/boardMain")
 	public void main() {
 		log.info("공지사항 페이지로 이동합니다.");
@@ -21,28 +20,15 @@ public class BoardController {
 	public void read() {
 		log.info("번의 공지사항을 읽습니다");
 	}
+	
+	@GetMapping("/boardWriter")
+	public void writer() {
+		log.info("공지사항 작성 페이지로 이동합니다.");
+	}
+	
+	
+	@GetMapping("/boardModify")
+	public void moidfy() {
+		log.info("공지사항 수정 페이지로 이동합니다.");
+	}
 }
-=======
-   @GetMapping("/boardMain")
-   public void main() {
-      log.info("                    ̵  մϴ .");
-   }
-   
-   @GetMapping("/boardRead")
-   public void read() {
-      log.info("         Խù            ̵ ");
-   }
-   
-   
-   @GetMapping("/boardWriter")
-   public void writer() {
-      log.info("          ۼ            ̵ ");
-   }
-   
-   
-   @GetMapping("/boardModify")
-   public void moidfy() {
-      log.info("                        ̵ ");
-   }
-}
->>>>>>> refs/remotes/origin/master
